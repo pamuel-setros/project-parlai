ac638f7318101920aeca7ee172a79219bbedb30c
Aidan Lin <aidanlin0730@gmail.com>
2026-03-01 21:48:24 +0000

    lins here! -a

--- scraper.py
+++ scraper.py
@@ -6,7 +6,31 @@ TEAM_SUBREDDITS = {
     "Golden State Warriors": "warriors",
     "Boston Celtics": "bostonceltics",
     "Milwaukee Bucks": "mkebucks",
-    # Teammates can add the rest later
+    "Atlanta Hawks" : "atlantahawks",
+    "Brooklyn Nets": "gonets",
+    "Charlotte Hornets": "charlottehornets",
+    "Chicago Bulls": "chicagobulls",
+    "Detroit Pistons": "detroitpistons",
+    "Indiana Pacers": "pacers",
+    "Miami Heat": "heat",
+    "New York Knicks": "NYKnicks",
+    "Orlando Magic": "orlandomagic",
+    "Philadelphia 76ers": "sixers",
+    "Toronto Raptors": "torontoraptors",
+    "Washington Wizards": "washingtonwizards",
+    "Dallas Mavericks": "mavericks",
+    "Denver Nuggets": "denvernuggets",
+    "Houston Rockets": "rockets",
+    "Los Angeles Clippers": "laclippers",
+    "Memphis Grizzlies": "memphisgrizzlies",
+    "Minnesota Timberwolves": "timberwolves",
+    "New Orleans Pelicans": "nolapelicans",
+    "Oklahoma City Thunder": "thunder",
+    "Phoenix Suns": "suns",
+    "Portland Trail Blazers": "ripcity",
+    "Sacramento Kings": "kings",
+    "San Antonio Spurs": "nbaspurs",
+    "Utah Jazz": "utahjazz"
 }
 
 def get_reddit_headlines(team_name):
